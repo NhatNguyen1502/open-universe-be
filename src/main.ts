5a27a5df4362770183d9f701f39395a9bbe3f606
nguyenlamnhat <zznhatphilong@gmail.com>
2024-03-05 15:59:50 +0700

    done config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('OpenU API')
+    .setDescription('The OpenU API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
+  console.log("App run on: http://localhost:3000");
 }
-bootstrap();
+bootstrap(); 
